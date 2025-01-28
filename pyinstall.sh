4be360b368d17c67c57a2e2f86889fb65e34ffc9
Graham Stark <graham.stark@virtual-worlds.biz>
2025-01-28 11:15:40 +0000

    Add files via upload

---
+++ pyinstall.sh
@@ -0,0 +1,19 @@
+#
+# create virtual environment in ~/pythonm
+# 
+sudo apt install python3-venv
+python3 -m venv ~/pythonm/
+#
+# install stuff in the environment using the local pip version:
+# juypterlab may be overkill
+# seaborn is a snazzier version of  Matplotlib https://seaborn.pydata.org/
+# 
+cd ~/pythonm/
+bin/pip install jupyterlab
+bin/pip install notebook
+bin/pip install seaborn
+bin/pip install pandas
+#
+# start the notebook: server
+#
+bin/jupyter notebook
